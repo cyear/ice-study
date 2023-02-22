ba6e6f85a8dfc39c864da780c2549326470b870e
cyear <1274210585@qq.com>
2023-02-22 13:50:19 +0800

    默认检测更新

--- core/ice.py
+++ core/ice.py
@@ -28,7 +28,7 @@ class ice_study:
         self.debug = res['debug']
         self.beta = res['beta']
         iLog_new(self,iLog)
-        Update(self.iLog, res['update'])
+        Update(self.iLog, True) #res['update'])
         if res['v']:
             self.iLog(self.VERSION)
             quit(0)
